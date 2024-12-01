72a25799e87e625685f6f0daaf4bf20b90f7e3a3
Mariekejd <146730379+Mariekejd@users.noreply.github.com>
2024-12-01 19:16:41 +0100

    push
    begin hamburger & responsive

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,11 @@
 // JavaScript Document
 console.log("hello");
 
+const HamburgerKnop = document.querySelector("header ul:first-of-type li:nth-of-type(1)")
+
+HamburgerKnop.onclick = openMenu
+
+function openMenu(){
+    const navHamburger = document.querySelector("header nav ul:nth-of-type(3)")
+    navHamburger.classList.add("toonMenu")
+}
