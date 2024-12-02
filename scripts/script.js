41bc5a4667219f43b4ab9a9b3ffbd82a6ecc46df
Mariekejd <146730379+Mariekejd@users.noreply.github.com>
2024-12-02 14:34:08 +0100

    -

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,12 @@
 // JavaScript Document
 console.log("hello");
 
-const HamburgerKnop = document.querySelector("header ul:first-of-type li:nth-of-type(1)")
+const HamburgerKnop = document.querySelector(".burgerKnop")
 
-HamburgerKnop.onclick = openMenu
+HamburgerKnop.addEventListener("click", openMenu)
 
 function openMenu(){
     const navHamburger = document.querySelector("header nav ul:nth-of-type(3)")
     navHamburger.classList.add("toonMenu")
+    console.log("hoi")
 }
