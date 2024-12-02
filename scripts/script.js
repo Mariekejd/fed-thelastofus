607b902927fc83d90ef62badb0af40c99f2f9b68
Mariekejd <146730379+Mariekejd@users.noreply.github.com>
2024-12-02 19:52:44 +0100

    hamburgertje

--- scripts/script.js
+++ scripts/script.js
@@ -2,11 +2,18 @@
 console.log("hello");
 
 const HamburgerKnop = document.querySelector(".burgerKnop")
+const sluitKnop = document.querySelector(".knopjeSluit")
 
 HamburgerKnop.addEventListener("click", openMenu)
+sluitKnop.addEventListener("click", sluitMenu)
 
 function openMenu(){
     const navHamburger = document.querySelector("header nav ul:nth-of-type(3)")
     navHamburger.classList.add("toonMenu")
     console.log("hoi")
+}
+
+function sluitMenu(){
+    const navHamburger = document.querySelector("header nav ul:nth-of-type(3)")
+    navHamburger.classList.remove("toonMenu")
 }
